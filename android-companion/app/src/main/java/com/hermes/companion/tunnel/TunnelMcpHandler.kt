a4a5cd644a1a1723314aca855544d5207b5730db
longkeyi002-coder <longkeyi002@gmail.com>
2026-09-04 07:20:32 +0800

    Import JSON accessors for tunnel handler

--- android-companion/app/src/main/java/com/hermes/companion/tunnel/TunnelMcpHandler.kt
+++ android-companion/app/src/main/java/com/hermes/companion/tunnel/TunnelMcpHandler.kt
@@ -10,6 +10,8 @@ import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonArray
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import kotlinx.serialization.json.put
 
 internal class TunnelMcpHandler(private val context: Context) {
