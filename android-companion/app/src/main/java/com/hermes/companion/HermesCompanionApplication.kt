f7b34e850668ebabffde86d6f4a21043d612f1b9
longkeyi002-coder <longkeyi002@gmail.com>
2026-09-03 21:45:45 +0800

    feat(android): start only selected local or cloud transport

--- android-companion/app/src/main/java/com/hermes/companion/HermesCompanionApplication.kt
+++ android-companion/app/src/main/java/com/hermes/companion/HermesCompanionApplication.kt
@@ -2,13 +2,15 @@ package com.hermes.companion
 
 import android.app.Application
 import com.hermes.companion.data.UploadWorker
+import com.hermes.companion.data.SettingsRepository
+import com.hermes.companion.local.LocalMcpServer
 import com.hermes.companion.push.HermesNotifications
 import com.hermes.companion.push.PushRegistration
 
 class HermesCompanionApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        UploadWorker.schedulePeriodic(this)
+        if (SettingsRepository(this).isLocalMode()) LocalMcpServer.start(this) else UploadWorker.schedulePeriodic(this)
         HermesNotifications.createChannel(this)
         PushRegistration.refresh(this)
     }
