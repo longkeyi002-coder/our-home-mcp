341771e300052f764803d4f0c6805afdc6ea69fa
longkeyi002-coder <longkeyi002@gmail.com>
2026-09-04 19:11:44 +0800

    feat(android): auto-connect default Runtime on app start (#22)

--- android-companion/app/src/main/java/com/hermes/companion/HermesCompanionApplication.kt
+++ android-companion/app/src/main/java/com/hermes/companion/HermesCompanionApplication.kt
@@ -1,6 +1,7 @@
 package com.hermes.companion
 
 import android.app.Application
+import com.hermes.companion.data.AutoConfiguration
 import com.hermes.companion.data.UploadWorker
 import com.hermes.companion.push.HermesNotifications
 import com.hermes.companion.push.PushRegistration
@@ -8,6 +9,7 @@ import com.hermes.companion.push.PushRegistration
 class HermesCompanionApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        AutoConfiguration.applyIfNeeded(this)
         UploadWorker.schedulePeriodic(this)
         UploadWorker.enqueueIfConfigured(this)
         HermesNotifications.createChannel(this)
